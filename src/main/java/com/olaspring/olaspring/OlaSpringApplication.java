6451bcee670c455f4bef9b2f46e46b4fb402e44b
William <william.fujimori@gmail.com>
2023-01-17 11:21:28 -0300

    feat: initial commit

---
+++ src/main/java/com/olaspring/olaspring/OlaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.olaspring.olaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olaspring/olaspring/OlaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olaspring.olaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
